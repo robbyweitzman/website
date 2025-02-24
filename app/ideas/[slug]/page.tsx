831433215873407a7d9b0164fb9637965be16f3e
robbyweitzman <robby.weitzman@gmail.com>
2025-02-23 22:41:05 -0500

    Update page.tsx

--- app/ideas/[slug]/page.tsx
+++ app/ideas/[slug]/page.tsx
@@ -46,14 +46,14 @@ const posts: BlogPost[] = [
       <br /><br />
       After going through a couple rolls, we developed them and they looked awesome. Not too long after, I decided to buy this domain to upload the pictures and create an online photo gallery.
       <br /><br />
-      As time went on, I continued taking pictures with my dad, but never made the website I wanted to put them on. When I saw Replit’s CEO <a href="https://x.com/amasad/status/1892805615766163529" className="custom-link">making a brick breaker game using Replit and Grok this week
+      As time went on, I continued taking pictures with my dad, but never made the website I wanted to put them on. When I saw Replit's CEO, Amjad Masad, <a href="https://x.com/amasad/status/1892805615766163529" className="custom-link">making a brick breaker game using Replit and Grok this week
 </a>, it opened my eyes to what you can really build with AI.
       <br /><br />
       I'm quite familiar with AI for certain use cases from both using it at Merge to help me automate sales processes/data entry, plus selling to new AI companies who need proprietary company data to run their enterprise search and agent products.
       <br /><br />
-      I knew you could use AI to code, but having no coding background, I thought it was just for small tweaks and not building entire websites. I was amazed what @amasad built so quickly, so decided to give it a go.
+      I knew you could use AI to code, but having no coding background, I assumed it was just for small tweaks and not launching entire websites. But after watching Amjad's, and with my 2025 mantra, "you can just do things" in mind, I decided to give it a go.
       <br /><br />
-      I loaded up Grok, and spoke to it in the same way I would a friend sitting next to me who is an expert in the field. I described what I wanted to build, how I wanted it to look, and where I wanted to deploy it.
+      I loaded up Grok, and spoke to it in the same way I would a friend sitting next to me. I described what I wanted to build, how I wanted it to look, and where I wanted to deploy it.
       <br /><br />
       I first tried Replit after seeing the above video, but landed on using v0.dev (an AI-powered tool to generate React UI from natural language) from Vercel because I wanted to prioritize a great looking front end.
       <br /><br />
